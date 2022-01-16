21a1c913c5da8f6fddeaa785a6ad94432d2e7e70
Joseph White <dignan89@gmail.com>
2022-01-15 16:52:06 -0800

    Add initial schema for systems tasking

--- app/app/models.py
+++ app/app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Command(models.Model):
+    command = models.CharField(max_length=255, unique=True, db_index=True, null=False)
+
+    def __str__(self):
+        return self.command
+
+
+class TaskApplication(models.Model):
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    command = models.ForeignKey(Command, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    task_application = models.ForeignKey(TaskApplication, on_delete=models.CASCADE)
+    status = models.CharField(max_length=200, choices=('new', 'in_progress', 'done'), default='new', db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
