05b373bdf87a96e8ae964d550d153258a60873df
Joseph White <dignan89@gmail.com>
2022-01-16 16:45:41 -0800

    Update urls to work with class based views

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
-
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from app import views
 
-
 urlpatterns = [
-    path('applications', views.application_list)
+    path('applications/', views.ApplicationList.as_view()),
+    path('applications/<int:pk>/', views.ApplicationDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
